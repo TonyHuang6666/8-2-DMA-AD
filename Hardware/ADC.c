74895832c53a0d86847e20bfaab98bc9aab986db
Tony_Huang <Tony_Huang@TONY_HUANG666>
2023-07-31 20:22:00 +0800

    添加注释

--- Hardware/ADC.c
+++ Hardware/ADC.c
@@ -12,7 +12,7 @@ void ADC_Initilize(void)
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
     // 3.设置软件触发
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_55Cycles5);
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_55Cycles5);// 通道，序号，采样时间
     ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 2, ADC_SampleTime_55Cycles5);
     ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 3, ADC_SampleTime_55Cycles5);
     ADC_RegularChannelConfig(ADC1, ADC_Channel_3, 4, ADC_SampleTime_55Cycles5);
@@ -26,21 +26,21 @@ void ADC_Initilize(void)
     ADC_InitStructure.ADC_NbrOfChannel = 4;                             // 通道数
     ADC_Init(ADC1, &ADC_InitStructure);
     // 5.使能ADC
-    //ADC_DMACmd(ADC1, ENABLE); // ADC触发DMA
+    //ADC_DMACmd(ADC1, ENABLE); 
     ADC_Cmd(ADC1, ENABLE);
     // 6.校准
-    ADC_ResetCalibration(ADC1);
-    while (ADC_GetResetCalibrationStatus(ADC1) == SET)
+    ADC_ResetCalibration(ADC1);//复位校准寄存器
+    while (ADC_GetResetCalibrationStatus(ADC1) == SET)//等待校准寄存器复位完成
         ;
-    ADC_StartCalibration(ADC1);
-    while (ADC_GetCalibrationStatus(ADC1) == SET)
+    ADC_StartCalibration(ADC1);//开始校准
+    while (ADC_GetCalibrationStatus(ADC1) == SET)//等待校准完成
         ;
 }
 void ADC_GetValue(void)
 {
     // 假如ADC是单次转换模式，DMA也是单次转运模式，则需要在每次转换完后重新设置DMA的传输计数器
     DMA_Cmd(DMA1_Channel1, DISABLE); // 在修改DMA1_Channel1->CNDTR寄存器前，必须先让DMA1_Channel1失能
-    DMA_SetCurrDataCounter(DMA1_Channel1, 4);
+    DMA_SetCurrDataCounter(DMA1_Channel1, 4);//设置传输计数器为4
     DMA_Cmd(DMA1_Channel1, ENABLE);
     ADC_SoftwareStartConvCmd(ADC1, ENABLE);
     while(DMA_GetFlagStatus(DMA1_FLAG_TC1)==RESET);//没转运完就等待
--- System/DMA.c
+++ System/DMA.c
@@ -4,17 +4,17 @@ void DMA_Initilize(void)
 {
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
     DMA_InitTypeDef DMA_InitStructure;
-    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;
-    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)AD_Value;
-    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
-    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
-    DMA_InitStructure.DMA_BufferSize = 4;
-    DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;//外设地址为ADC1的DR寄存器
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;//ADC1->DR是16位的，所以这里是半字
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;//外设地址不变
+    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)AD_Value;//存放ADC值的数组
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;//存放ADC值的数组是16位的，所以这里是半字
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;//存放ADC值的数组递增
+    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;//外设作为数据源
+    DMA_InitStructure.DMA_BufferSize = 4;//DMA缓存大小为4
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;//单次转运模式，不循环
     DMA_InitStructure.DMA_M2M = DMA_M2M_Disable; // 不是软件触发，而是ADC1触发
-    DMA_InitStructure.DMA_Priority = DMA_Priority_Medium;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_Medium;//DMA优先级中等
     DMA_Init(DMA1_Channel1, &DMA_InitStructure); // 硬件触发，通道不可以任意选择
     DMA_Cmd(DMA1_Channel1, ENABLE);
     ADC_DMACmd(ADC1, ENABLE); // ADC触发DMA
